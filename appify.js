9e0f2800dd6774ffdb72450c5ed40f975ea29ed7
Jayhunsu <hunsuebun@gmail.com>
2024-07-10 13:31:38 +0100

    Add files via upload
    Playlist recommender

---
+++ appify.js
@@ -0,0 +1,25 @@
+// const redirect_url = "http://127.0.0.1:5500/appify.html"
+
+// const clientId = "";
+// const clientSecret = "";
+
+// function onPageLoad () {
+
+// }
+// const AUTHORIZE = "https://accounts.spotify.com/authorize"
+
+
+// function requestAuthorization () {
+//     clientId = document.getElementById('clientId').value
+//     clientSecret = document.getElementById('clientSecret').value
+//     localStorage.setItem['clientId', clientId];
+//     localStorage.setItem['clientSecret', clientSecret];
+
+//     let url = AUTHORIZE;
+//     url += "?clientId" + clientId
+//     url += "&response_type=code";
+//     url += "&redirect_url=" + encodeURI(redirect_url)
+//     url += "&show_dialog=true";
+//     url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private"
+// }
+
